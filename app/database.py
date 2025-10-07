8ff69846ca12c4ad3d0f9ec2bc04ae919a40f285
Mel1441 <masonlm68@gmail.com>
2025-10-07 13:47:30 -0400

    fix env vars

--- app/database.py
+++ app/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 from dotenv import load_dotenv
-env_file = ".env.prod" if os.getenv("ENV") == "prod" else ".env.dev"
+
+env_file = ".env.production" if os.getenv("ENV") == "production" else ".env.development"
 load_dotenv(dotenv_path=env_file)
 
 DB_URL = os.getenv("DATABASE_URL")
