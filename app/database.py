764ba96097362c00dd9e586ff92c8bb432261ce9
Mel1441 <masonlm68@gmail.com>
2025-10-07 14:12:39 -0400

    wrap in function

--- app/database.py
+++ app/database.py
@@ -6,9 +6,15 @@ from dotenv import load_dotenv
 env_file = ".env.development" if os.getenv("ENV") == "development" else ".env.production"
 load_dotenv(dotenv_path=env_file)
 print(env_file)
-DB_URL = os.getenv("DATABASE_URL")
-print(DB_URL)
-engine = create_engine(DB_URL, echo=True, future=True)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+def get_engine():
+    db_url = os.getenv("DATABASE_URL")
+    if not db_url:
+        raise ValueError("DATABASE_URL not set")
+    return create_engine(db_url, echo=True, future=True)
+
+#DB_URL = os.getenv("DATABASE_URL")
+#print(DB_URL)
+#engine = create_engine(DB_URL, echo=True, future=True)
+SessionLocal = sessionmaker(bind=get_engine(), autocommit=False, autoflush=False)
 
 Base = declarative_base()
