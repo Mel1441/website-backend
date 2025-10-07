7f06021ab1050272a0a3692166bae7cc4c48d6dc
Mel1441 <masonlm68@gmail.com>
2025-10-07 13:21:47 -0400

    fix url

--- app/config/cors.py
+++ app/config/cors.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def add_cors(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173", "https://home.mmason.tech"],
+        allow_origins=["http://localhost:5173", "https://mmason.tech"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
