36f96b6feb21959217e1c731224d85403b664577
Mel1441 <masonlm68@gmail.com>
2025-10-08 12:43:17 -0400

    add api url

--- app/config/cors.py
+++ app/config/cors.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def add_cors(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173", "https://mmason.tech"],
+        allow_origins=["http://localhost:1000", "https://mmason.tech", "https://api.mmason.tech"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
